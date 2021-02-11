360c2b09d9ae43e6293529a9443056bf14d43206
kenta zaibu <smtwkt@yahoo.co.jp>
2021-02-11 11:46:47 +0000

    user registration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,9 @@
     <div class="center jumbotron">
         <div class="text-center">
             <h1>Welcome to the Microposts</h1>
+            
+            {{ -- ユーザ登録ページのリンク--}}
+            { !! link_to_route('singup.get','sing up now!',[],['class='btn btn-lg btn-primary']) !! }
         </div>
     </div>
 @endsection
